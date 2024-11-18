ecfaf5bb6a9cebb141408b6760f9784f4b95b179
BabiSato <barbaranayumisato@gmail.com>
2024-11-18 19:46:24 -0300

    atualiza server e database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,10 @@
 import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const connectionString = 'postgresql://postgres:xXQyMYHbJWkzoKRjVdsqCSnGNmCxSTWs@junction.proxy.rlwy.net:25852/railway';
+const connectionString = process.env.DATABASE_URL;
+
+// const connectionString = 'postgresql://postgres:xXQyMYHbJWkzoKRjVdsqCSnGNmCxSTWs@junction.proxy.rlwy.net:25852/railway';
 
 const pool = new Pool({
   connectionString,
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import userRoutes from "./routes/authRoutes";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
